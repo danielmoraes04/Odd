cad82bc214714b390558c0d0ed05e3656f5bd459
Your Name <you@example.com>
2024-08-28 21:43:57 -0300

    Aula 1 do git

---
+++ daniel/daniel/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for daniel project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+from django.core.asgi import get_asgi_application
+from dj_static import Cling, MediaCling
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'daniel.settings')
+
+application = Cling(MediaCling(get_asgi_application()))
